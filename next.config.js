d04611f433ffa0d55669fea78b5da9311e93844e
webersilv4 <webercost4@hotmail.com>
2022-11-27 20:58:48 -0300

    Alter config Cors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     reactStrictMode: true,
-}
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://gas-station-lemon.vercel.app/:path*',
+          },
+        ]
+      },
+  };
