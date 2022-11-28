836f1c5db9edfbbd5bdfb2911be2af8802930801
webersilv4 <webercost4@hotmail.com>
2022-11-27 21:51:55 -0300

    Cors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
     reactStrictMode: true,
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://gas-station-smoky.vercel.app/api/:path*',
-          },
-        ]
-      },
   };
