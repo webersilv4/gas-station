0b28c2adc6b8f3fd4fea8d5ce34cffbb82d7291a
webersilv4 <webercost4@hotmail.com>
2022-11-27 21:49:15 -0300

    Cors

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         return [
           {
             source: '/api/:path*',
-            destination: 'https://gas-station-smoky.vercel.app/api/*',
+            destination: 'https://gas-station-smoky.vercel.app/api/:path*',
           },
         ]
       },
